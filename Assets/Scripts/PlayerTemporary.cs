45e6be9bd2735a7f280b4129a39764eef75567be
pogdog <76798720+realpogdog@users.noreply.github.com>
2026-04-02 23:56:28 +0900

    plrHandler

---
+++ Assets/Scripts/PlayerTemporary.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerTemporary : MonoBehaviour
+{
+    /* < The beginning of the script >
+    
+    +  Stuffs you can do here : 
+
+    Variables Setting ( mijisu setting )
+    List Setting
+    Seriallize Fields 
+
+    The example form of variable definition:
+    [ ACCESS MODIFIER ] / [ OTHER MODIFIER ] / [ DATA TYPE ] / [ DATA NAME ] = [ DATA VALUE ];
+    
+    */
+
+    void Awake()
+    {
+     /*
+
+     < The first ever running function whenever you start the unity >
+
+     + Stuffs you can do here: 
+
+     [ DATA NAME ] = [ REFERENCE ];
+
+     [ DATA NAME ] = [ INITIAL VALUE ];
+
+     */
+
+      
+    }
+
+
+    void Update()
+    {
+        // runs every per frame
+    }
+
+    void FixedUpdate()
+    {
+        // faster than physics
+    }
+
+    void LateUpdate()
+    {
+        // The inner last Update function
+    }
+}
