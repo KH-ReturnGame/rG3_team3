a6141600fc94f88f1700e532fe8a5ef1db0771cc
ef0083 <hchoi2983@gmail.com>
2026-04-10 03:55:21 +0900

    Scene setting

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    int gold;
+    int exp;
+    int difficulty;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Skill1()
+    {
+
+    }
+
+    void Skill2()
+    {
+
+    }
+
+    void Skill3()
+    {
+
+    }
+}
