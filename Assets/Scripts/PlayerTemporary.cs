9166bc54c470caebf9a757ae501893fa816900bc
pogdog <76798720+realpogdog@users.noreply.github.com>
2026-04-10 03:23:09 +0900

    좌우이동/점프/어퍼컷/다운슬램/넉백/히트박스기능/디바운스 및 데이터리스트관리/ 등등 소소한 요소들 제작

--- Assets/Scripts/PlayerTemporary.cs
+++ Assets/Scripts/PlayerTemporary.cs
@@ -4,69 +4,231 @@ using UnityEngine;
 
 public class PlayerTemporary : MonoBehaviour
 {
-    /* < The beginning of the script >
-    
-    +  Stuffs you can do here : 
-
-    Variables Setting ( mijisu setting )
-    List Setting
-    Seriallize Fields 
-
-    The example form of variable definition:
-    [ ACCESS MODIFIER ] / [ OTHER MODIFIER ] / [ DATA TYPE ] / [ DATA NAME ] = [ DATA VALUE ];
-    
-    */
-
     private Rigidbody2D rb2d;
     private SpriteRenderer sprdr;
+    private Animator animtr;
+
+    private float movementSpeed = 10;
+    private float jumpForce = 25f;
 
 
-    private float movementSpeed = 30;
-    
+
     private float moveInput;
 
+    public List<string> Stunlist = new List<string>();
+    public List<string> Cooldownlist = new List<string>();
+
+    public string PressingKey;
+
+    private int Combo = 0;
+    private float lastComboChangeTime;
+
+    private bool isUptilt;
+    private bool isDowntilt;
+
+
+    public bool isDebounce = false;
+
+
+
+
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius = 0.2f;
+    public LayerMask whatIsGround;
 
     void Awake()
     {
-     /*
-
-     < The first ever running function whenever you start the unity >
-
-     + Stuffs you can do here: 
-
-     [ DATA NAME ] = [ REFERENCE ];
-
-     [ DATA NAME ] = [ INITIAL VALUE ];
-
-     */
-
-    rb2d = GetComponent<Rigidbody2D>();
-    sprdr = GetComponent<SpriteRenderer>();
-
+        rb2d = GetComponent<Rigidbody2D>();
+        sprdr = GetComponent<SpriteRenderer>();
+        animtr = GetComponent<Animator>();
     }
-
 
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-         if (moveInput > 0)
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    PressingKey = key.ToString();
+                }
+            }
+        }
+
+        isUptilt = Input.GetKey(KeyCode.W);
+        isDowntilt = Input.GetKey(KeyCode.S);
+
+         if (!Stunlist.Contains("Stun"))
+        {
+            moveInput = Input.GetAxisRaw("Horizontal");
+            
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpForce);
+            }
+        }
+
+        if (moveInput > 0)
             sprdr.flipX = false;
         else if (moveInput < 0)
             sprdr.flipX = true;
 
-        // runs every per frame
+        if (moveInput != 0)
+            animtr.SetBool("isWalking", true);
+        else
+            animtr.SetBool("isWalking", false);
+
+        if (Combo != 0 && Time.time - lastComboChangeTime >= 1f)
+        {
+            Combo = 0;
+            Debug.Log("COMBO RESET DUE TO TIMER");
+        }
+
+        if (Combo == 3)
+        {
+            AddDataToList("LightCooldown", Cooldownlist, 0.7f);
+            Combo = 0;
+            Debug.Log("COMBO RESET DUE TO END OF STACK");
+        }
+
+        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.L)) && !isDebounce)
+        {
+            if (!Cooldownlist.Contains("LightCooldown"))
+            {
+                isDebounce = true;
+                Combo++;
+                lastComboChangeTime = Time.time;
+
+                Debug.Log(Combo);
+
+
+                if (!isGrounded && isDowntilt)
+                {
+                    StartCoroutine(ExecuteAttack(new Vector2(0f, -2f), new Vector2(8f, 4f), 0.15f, 40f, true));
+                }
+                else
+                {
+                    StartCoroutine(ExecuteAttack(new Vector2(2f, 0.5f), new Vector2(9f, 6f), 0.1f, 30f, false));
+                }
+
+                AddDataToList("LightCooldown", Cooldownlist, 0.26f);
+                StartCoroutine(ResetDebounce(0.26f));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !isDebounce)
+        {
+            if (!Cooldownlist.Contains("Skill1CD"))
+            {
+                isDebounce = true;
+                Debug.Log("Skill 1 Activated");
+                AddDataToList("Skill1CD", Cooldownlist, 3f);
+                StartCoroutine(ResetDebounce(0.5f));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && !isDebounce)
+        {
+            if (!Cooldownlist.Contains("Skill2CD"))
+            {
+                isDebounce = true;
+                Debug.Log("Skill 2 Activated");
+                AddDataToList("Skill2CD", Cooldownlist, 5f);
+                StartCoroutine(ResetDebounce(0.5f));
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        // faster than physics
-        rb2d.linearVelocity = new Vector2(moveInput * movementSpeed, rb2d.linearVelocity.y);
-
+        if (!Stunlist.Contains("Stun"))
+            rb2d.linearVelocity = new Vector2(moveInput * movementSpeed, rb2d.linearVelocity.y);
     }
 
-    void LateUpdate()
+    private IEnumerator ResetDebounce(float delay)
     {
-        // The inner last Update function
+        yield return new WaitForSeconds(delay);
+        isDebounce = false;
+    }
+
+    private void AddDataToList(string dataName, List<string> dataList, float duration)
+    {
+        StartCoroutine(Activate());
+        IEnumerator Activate()
+        {
+            dataList.Add(dataName);
+            yield return new WaitForSeconds(duration);
+            dataList.Remove(dataName);
+        }
+    }
+
+    private IEnumerator ExecuteAttack(Vector2 offset, Vector2 size, float duration, float damage, bool isDownSmash)
+    {
+        float dir = sprdr.flipX ? -1f : 1f;
+
+        if (isDownSmash)
+        {
+            rb2d.linearVelocity = new Vector2(0, -40f); 
+        }
+        else
+        {
+            rb2d.linearVelocity = new Vector2(0, rb2d.linearVelocity.y);
+            rb2d.AddForce(new Vector2(dir * 40f, 0), ForceMode2D.Impulse);
+        }
+
+        float elapsed = 0f;
+        HashSet<Collider2D> hitHistory = new HashSet<Collider2D>();
+
+        while (elapsed < duration)
+        {
+            float currentDir = sprdr.flipX ? -1f : 1f;
+            Vector2 pos = (Vector2)transform.position + new Vector2(offset.x * currentDir, offset.y);
+
+            Collider2D[] hits = Physics2D.OverlapBoxAll(pos, size, 0f, LayerMask.GetMask("Enemy"));
+
+            foreach (var hit in hits)
+            {
+                if (!hitHistory.Contains(hit))
+                {
+                    Debug.Log("JUST HIT: " + hit.name);
+                    hitHistory.Add(hit);
+
+                    if (hit.TryGetComponent(out Rigidbody2D enemyRb))
+                    {
+                       if (isDownSmash)
+                        {
+        
+                            enemyRb.linearVelocity = Vector2.zero;
+                            enemyRb.AddForce(Vector2.down * 140f, ForceMode2D.Impulse);
+                            Debug.Log("DOWN SMASH!");
+                        }
+                        else if (isUptilt)
+                        {
+                            enemyRb.linearVelocity = Vector2.zero;
+                            enemyRb.AddForce(Vector2.up * 25f, ForceMode2D.Impulse);
+                        }
+                        else
+                        {
+                            enemyRb.AddForce(new Vector2(dir * 10f, 0), ForceMode2D.Impulse);
+                        }
+                    }
+                }
+            }
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (sprdr == null) return;
+        Gizmos.color = Color.red;
+        float dir = sprdr.flipX ? -1f : 1f;
+        Gizmos.DrawWireCube(transform.position + new Vector3(2f * dir, 0.5f, 0), new Vector3(9f, 6f, 1f));
     }
 }
