db955a99b8fdece94ec8156f9bc38f614a79f5cb
pogdog <76798720+realpogdog@users.noreply.github.com>
2026-04-12 23:02:31 +0900

    유틸리티 일반화

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,16 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Utilities : MonoBehaviour
+// 다기능 도구 집합 패키지 / 다른 스크립트에서도 불러와서 사용가능함!
+namespace Utility.DataManagement
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public static class ListManagement
     {
-        
-    }
+        private class CoroutineRunner : MonoBehaviour { }
+        private static CoroutineRunner _runner;
+        private static CoroutineRunner Runner
+        {
+            get
+            {
+                if (_runner == null)
+                {
+                    GameObject obj = new GameObject("[ListManagement_Runner]");
+                    _runner = obj.AddComponent<CoroutineRunner>();
+                    Object.DontDestroyOnLoad(obj);
+                }
+                return _runner;
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        public static string AddData(string dataName, List<string> dataList, float duration = 0f)
+        {
+            if (duration > 0)
+            {
+                Runner.StartCoroutine(ActivateTimer(dataName, dataList, duration));
+                return "타이머 시작";
+            }
+            else
+            {
+                dataList.Add(dataName);
+                return dataName;
+            }
+        }
+
+        private static IEnumerator ActivateTimer(string dataName, List<string> dataList, float duration)
+        {
+            dataList.Add(dataName);
+            yield return new WaitForSeconds(duration);
+            dataList.Remove(dataName);
+        }
     }
 }
